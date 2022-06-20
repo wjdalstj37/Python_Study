0111fefa5a6f803b9d5bb8c4a57d2590f8a93bd3
wjdalstj37 <jms6020@gmail.com>
2022-06-20 18:09:10 +0900

    third

---
+++ chap03/01.py
@@ -0,0 +1,4 @@
+a = int(input("정수를 입력하세요: "))
+b = float(input("실수를 입력하세요: "))
+print("두 수를 더한 결과는: ", a+b, "입니다.")
+print(type(a+b))
---
+++ chap03/03.py
@@ -0,0 +1,11 @@
+a = "Hello groom!"
+if(a == True) :
+    print("참")
+else:
+    print("거짓")
+
+b = True
+if(b == True):
+    print("참")
+else:
+    print("거짓")
